1e71c360e9e271e1e5049ffc61a3dcb359ed726b
EliasAras <elias.aras@edu.ece.fr>
2018-03-02 11:25:02 +0100

    Envoie Main

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void Recuperation()
+{
+//    std::string fic ("Influence.txt");
+//
+//    std::ifstream fichier(fic.c_str(), std::ios::in);
+
+//    if(fichier)
+//    {
+//        do
+//        {
+//
+//        }
+//    }
+}
+
+int main()
+{
+
+    return 0;
+}
